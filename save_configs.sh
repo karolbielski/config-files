2de5b5f1f5d11fc2eaed44ce4f559fe58dd24574
Karol Bielski <karol.bielski.dev@gmail.com>
2025-05-29 18:05:29 +0200

    Update docs

---
+++ save_configs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp ~/.aerospace.toml aerospace/.aerospace.toml
+echo "AeroSpace config saved"
+
+cp ~/.config/borders/bordersrc jankyborders/bordersrc
+echo "JankyBorders config saved"
+
+cp -a ~/.config sketchybar 
+echo "SketchyBar config saved" 
+
+cp -a ~/.config karabiner 
+echo "Karabiner-Elements config saved"
+
